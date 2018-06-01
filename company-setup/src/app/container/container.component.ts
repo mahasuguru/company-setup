3c645f888b7d794c2905c27cff39c8644404dec8
maha-app1 <maha.suguru@gmail.com>
2018-06-01 17:04:48 +0530

    initial commit

--- company-setup/src/app/container/container.component.ts
+++ company-setup/src/app/container/container.component.ts
@@ -6,35 +6,47 @@ import { Container } from './container';
   templateUrl: './container.component.html',
   styleUrls: ['./container.component.scss']
 })
+
 export class ContainerComponent implements OnInit {
+  public inputHeight: String;
+
   errorMsgList: any;
-  constructor() { }
-public container=new Container();
+
+  constructor() {
+  }
+
+  public container = new Container();
+
   ngOnInit() {
     console.log(this.container);
   }
-cancel(){
-
-  alert('Are you Sure to Cancel Company Setup ?');
-}
-save(){
-let containerObj=this.container;
-this.errorMsgList=[];
-  containerObj.company_name === '' ? this.errorMsgList.push('company Name is required.'):'';
-  containerObj.company_no === '' ? this.errorMsgList.push('company_no is required'):'';
-  containerObj.address === '' ? this.errorMsgList.push('address is required.'):'';
-  containerObj.stateId === '' ? this.errorMsgList.push('stateId is required'):'';
 
 
-  if(this.errorMsgList.length==0){
-    alert('are u want save CompanySetupPage')
+  cancel() {
+
+    alert('Are you Sure to Cancel Company Setup ?');
   }
-  else{
-    this.errorMsgList.push('field required...!')
 
+  save() {
+    let containerObj = this.container;
+    this.errorMsgList = [];
+    containerObj.company_name === '' ? this.errorMsgList.push('company Name is required.') : '';
+    containerObj.company_no === '' ? this.errorMsgList.push('company_no is required') : '';
+    containerObj.address === '' ? this.errorMsgList.push('address is required.') : '';
+    containerObj.stateId === '' ? this.errorMsgList.push('stateId is required') : '';
+
+
+    if (this.errorMsgList.length == 0) {
+      alert('are u want save CompanySetupPage')
+    }
+    else {
+      this.errorMsgList.push('field required...!')
+
+    }
   }
 }
-}
+
+
 
 
 
